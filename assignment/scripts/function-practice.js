fe871c6529c53696bbc66fa11a6cd8ebcd5618f5
David Vetter <ddvetter23@gmail.com>
2022-08-10 18:15:50 -0500

    added some notes, adjusted words on couple answers

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -17,7 +17,7 @@ console.log('Test - should say "Hello World!"', hello());
 function helloName( name ) {
   let myName = name; // set variable myName = to argument
   return myName;
-}
+} // end helloName function
 // Remember to call the function to test
 console.log('Hello there, ', helloName('David'));
 
@@ -25,17 +25,17 @@ console.log('Hello there, ', helloName('David'));
 function addNumbers( firstNumber, secondNumber ) {
   let total = firstNumber + secondNumber; // add arguments
   return total; // return firstNumber + secondNumber;
-}
+} // end addNumbers function
 
 console.log('The total of the two numbers is: ', addNumbers(6, 4)); // console log to test function
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree(num1, num2, num3){
   let sum = (num1 * num2 * num3); // multiply the 3 arguments
-  return sum; // return the sum of the 3 numbers
-}
+  return sum; // return the product of the 3 numbers
+} // end multiplyThree function
 
-console.log('The sum of the three numbers is: ', multiplyThree(2, 3, 5)); // log to test function
+console.log('The total after multiplying 2*3*5 is: ', multiplyThree(2, 3, 5)); // log to test proper function
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
@@ -45,47 +45,48 @@ function isPositive( number ) {
   } else {
     return false;
   }
-}
+} // end isPositive function
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
+console.log( 'isPositive - should say true', isPositive(3) ); // logs for proof of functionality
 console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-let testArray = ['David', 'Austin', 'Heinrich', 'Wanda'];
-let empArray = [];
+let testArray = ['David', 'Austin', 'Heinrich', 'Wanda']; // test array
+let empArray = []; // test empty array
 function getLast( array ) {
-  let arr = array;
-  return arr[arr.length - 1];
-}
+  let arr = array; // create variable for argument
+  return arr[arr.length - 1]; // return the item at the index of array length minus 1
+} // end getLast function
 
-console.log('The last item in a hard coded array is ', getLast([3,4,5]));
-console.log('The last item when array is empty: ', getLast([]));
+console.log('The last item in a hard coded array is ', getLast([3,4,5])); // console logs to show
+console.log('The last item when array is empty: ', getLast([])); // this works
 console.log(`The last item when an array is empty from array by name is ${getLast(empArray)}`);
 console.log(`The last item in the array from an array by name is ${getLast(testArray)}`);
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-let arr7 = [2, 3, 4, 5, 6, 7];
-let arrString = ['Red', 'Green', 'Orange', 'Purple'];
+let arr7 = [2, 3, 4, 5, 6, 7]; // test array
+let arrString = ['Red', 'Green', 'Orange', 'Purple']; // test array with strings
 function find( value, array ){
-  let arr = array;
-  let target = value;
-  for (item of arr) {
-    if (item === target) {
+  let target = value; // create variable for first argument
+  let arr = array; // create variable for second argument
+  for (item of arr) { // loop to check the item in arr
+    if (item === target) { // if to check if equal to first argument
       return true;
     }
   }
   return false;
-}
+} // end find function
 
 console.log(`We are looking for the number 75, is it here? ${find(75, [32, 57, 63, 75])}`);
-console.log('We are looking for the number 6, is it here?', find(6, arr7));
-console.log('We are looking for the number 8, is it here?', find(8, arr7));
+console.log('We are looking for the number 6, is it here?', find(6, arr7)); // console logs to show
+console.log('We are looking for the number 8, is it here?', find(8, arr7)); // functioning function
 console.log('We are looking for Orange, is it here?', find('Orange', arrString));
 console.log('We are looking for Blue, is it here?', find('Blue', arrString));
 console.log('We are looking for Black, is it here?', find('Black', ['Yellow', 'Pink', 'Brown', 'Blue']));
